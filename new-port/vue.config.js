a27b4c66ea0c0d78339a44fbc04079315f38ee50
Suthep Chanchuphol <moo_suthep@hotmail.com>
2019-05-22 00:26:16 +0700

    init vue

---
+++ new-port/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
