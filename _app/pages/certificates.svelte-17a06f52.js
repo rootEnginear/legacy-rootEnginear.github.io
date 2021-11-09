8320a3b819213af96cc524ad2779e7344d9ab513
rootEnginear <suthep.chanchuphol@outlook.com>
2021-11-09 15:10:58 +0000

    Update master to output generated at bed2ce4

--- _app/pages/certificates.svelte-17a06f52.js
+++
@@ -1,7 +0,0 @@
-import{S,i as b,s as A,e as d,t as c,c as p,a as u,g as h,d as l,U as o,b as P,f as C,D as n,E as v}from"../chunks/vendor-309969dd.js";function I(w){let t,e,s,m,f,y,r,_,g;return{c(){t=d("p"),e=d("span"),s=d("span"),m=c("Oh no!"),f=c(`
-		This page is under construction (\xB4\u3002\uFF3F\u3002\uFF40)`),y=c(`
-	While you're here, you can check out
-	`),r=d("a"),_=c("my LinkedIn"),g=c(" instead!"),this.h()},l(i){t=p(i,"P",{style:!0});var a=u(t);e=p(a,"SPAN",{style:!0});var k=u(e);s=p(k,"SPAN",{style:!0});var x=u(s);m=h(x,"Oh no!"),x.forEach(l),f=h(k,`
-		This page is under construction (\xB4\u3002\uFF3F\u3002\uFF40)`),k.forEach(l),y=h(a,`
-	While you're here, you can check out
-	`),r=p(a,"A",{href:!0});var E=u(r);_=h(E,"my LinkedIn"),E.forEach(l),g=h(a," instead!"),a.forEach(l),this.h()},h(){o(s,"font-weight","700"),o(e,"display","block"),o(e,"font-size","1.5rem"),P(r,"href","https://www.linkedin.com/in/suthep-ch/"),o(t,"text-align","center"),o(t,"margin-top","3rem"),o(t,"margin-bottom","3rem")},m(i,a){C(i,t,a),n(t,e),n(e,s),n(s,m),n(e,f),n(t,y),n(t,r),n(r,_),n(t,g)},p:v,i:v,o:v,d(i){i&&l(t)}}}class N extends S{constructor(t){super();b(this,t,null,I,A,{})}}export{N as default};
