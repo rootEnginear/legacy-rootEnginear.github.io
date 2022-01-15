484d64dadc9d2a4a9bbaca2c302a88cad9ca65ff
rootEnginear <suthep.chanchuphol@outlook.com>
2022-01-15 21:33:43 +0700

    feat: add qemmet in side project

--- src/data/side_projects.js
+++ src/data/side_projects.js
@@ -1,5 +1,12 @@
 const SIDE_PROJECTS = [
 	{
+		title: 'Qemmet',
+		link: 'https://rootenginear.github.io/Qemmet',
+		description:
+			'A shorthand notation for generating a quantum circuit and a quantum circuit diagram in Qiskit, QASM3, SVG, and PNG.',
+		keywords: ['Quantum Circuit', 'Quantum Circuit Diagram', 'Qiskit', 'QASM3', 'TypeScript']
+	},
+	{
 		title: 'KMUTT PEP Enhancer',
 		link: 'https://github.com/rootEnginear/KMUTT-PEP-Enhancer',
 		description:
@@ -8,7 +15,7 @@ const SIDE_PROJECTS = [
 	},
 	{
 		title: 'Kraichana?',
-		link: 'https://hashtagselfie.github.io/kraichana/',
+		link: 'https://hashtagselfie.github.io/kraichana',
 		description: 'A Vue.js progressive web application that improves ThaiChana user experience.',
 		keywords: ['SCSS', 'Vue.js (CLI, Router, Vuex, PWA)', 'User experience']
 	}
