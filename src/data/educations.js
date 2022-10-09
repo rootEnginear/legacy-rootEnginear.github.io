023a6f40de7488d419d2eb50d42a64d05d8973f5
rootEnginear <rootEnginear@users.noreply.github.com>
2022-10-09 23:45:01 +0700

    fix: edit education

--- src/data/educations.js
+++ src/data/educations.js
@@ -1,11 +1,7 @@
 const EDUCATIONS = [
 	{
 		name: "King Mongkut's University of Technology Thonburi",
-		description: [
-			"Studying bachelor's degree.",
-			'Majoring in computer engineering.',
-			'(Graduating in January 2023)'
-		],
+		description: ["Studying bachelor's degree.", 'Majoring in computer engineering.'],
 		gpa: 3.3,
 		graduate: true
 	},
